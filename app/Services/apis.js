2c78b2d2645aa45b2d10cc8ffc24df5ba424cb9b
Nicanor <nicanor200026@gmail.com>
2024-01-03 12:56:03 -0500

    add apis

--- app/Services/apis.js
+++ app/Services/apis.js
@@ -1,37 +1,39 @@
-const axios = require('axios').default;
-const { API } = require('./tools');
-
+const axios = require("axios").default;
+const { API } = require("./tools");
 
 const authentication = {
-    config: (options = {}) => {
-        for (let opt in options) {
-            axios.defaults.headers.common[opt] = options[opt]; 
-        }
-    },
-    get: async (path, config) => {
-        return axios.get(`${API.API_AUTHENTICATION}/${path}`, config);
-    },
-    post: (path, data = {}, config) => {
-        return axios.post(`${API.API_AUTHENTICATION}/${path}`, data, config);
-    },
-}
+  config: (options = {}) => {
+    for (let opt in options) {
+      axios.defaults.headers.common[opt] = options[opt];
+    }
+  },
+  get: async (path, config) => {
+    const api = `${API.API_AUTHENTICATION}/${path}`;
+    console.log(api);
+    return axios.get(api, config);
+  },
+  post: (path, data = {}, config) => {
+    const api = `${API.API_AUTHENTICATION}/${path}`;
+    console.log(api);
+    return axios.post(api, data, config);
+  },
+};
 
 const signature = {
-    config: (options = {}) => {
-        for (let opt in options) {
-            axios.defaults.headers.common[opt] = options[opt]; 
-        }
-    },
-    get: async (path, config) => {
-        return axios.get(`${API.API_SIGNATURE}/${path}`, config);
-    },
-    post: (path, data = {}, config) => {
-        return axios.post(`${API.API_SIGNATURE}/${path}`, data, config);
+  config: (options = {}) => {
+    for (let opt in options) {
+      axios.defaults.headers.common[opt] = options[opt];
     }
-}
+  },
+  get: async (path, config) => {
+    return axios.get(`${API.API_SIGNATURE}/${path}`, config);
+  },
+  post: (path, data = {}, config) => {
+    return axios.post(`${API.API_SIGNATURE}/${path}`, data, config);
+  },
+};
 
-
-module.exports = { 
-    authentication, 
-    signature  
-}
+module.exports = {
+  authentication,
+  signature,
+};
